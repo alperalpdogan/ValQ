2302da882fec3f964068fae0382496aaa5078941
ALPER ALPDOĞAN <alperalpdogan@posta.mu.edu.tr>
2022-01-10 16:50:59 +0300

    Migration

---
+++ Libraries/ValQ.Data/Migrations/20220110130030_AddedNewFields.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ValQ.Data.Migrations
+{
+    public partial class AddedNewFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "AspNetUsers",
+                newName: "NickName");
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfCorrectAnswers",
+                table: "Match",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfIncorrectAnswers",
+                table: "Match",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumberOfCorrectAnswers",
+                table: "Match");
+
+            migrationBuilder.DropColumn(
+                name: "NumberOfIncorrectAnswers",
+                table: "Match");
+
+            migrationBuilder.RenameColumn(
+                name: "NickName",
+                table: "AspNetUsers",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
